1551f872d54625d3cf71660f86b34de02e441ca1
kevin-rodriguez-dev <kevinrodrihid2003@gmail.com>
2024-09-27 15:55:54 -0600

    Add primary key constraint to model_has_permissions table

--- database/migrations/2019_10_07_232902_create_permission_tables.php
+++ database/migrations/2019_10_07_232902_create_permission_tables.php
@@ -32,17 +32,19 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames) {
             $table->unsignedInteger('permission_id');
+
             $table->string('model_type');
             $table->unsignedBigInteger($columnNames['model_morph_key']);
-        
+            $table->index([$columnNames['model_morph_key'], 'model_type', ], 'model_has_permissions_model_id_model_type_index');
+
             $table->foreign('permission_id')
                 ->references('id')
                 ->on($tableNames['permissions'])
                 ->onDelete('cascade');
-        
-            $table->primary(['permission_id', $columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_primary');
+
+            $table->primary(['permission_id', $columnNames['model_morph_key'], 'model_type']/* ,
+                    'model_has_permissions_permission_model_type_primary' */);
         });
-        
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
             $table->unsignedInteger('role_id');
